07b461224600e127cb805d931409f8e948b24680
EidalPRO <eidaloaxaca@gmail.com>
2024-11-09 21:10:46 -0600

    tema oscuri y claro

--- resources/views/sin-auth/galeria.blade.php
+++ resources/views/sin-auth/galeria.blade.php
@@ -16,12 +16,86 @@
 @section('style')
 <style>
     body {
-        color: var(--default-color);
+        color: #fff;
         background-color: var(--background-color);
         font-family: var(--default-font);
         background: url(assets/img/bg.jpg) top center no-repeat;
         background-size: cover;
         position: relative;
+    }
+
+    /* From Uiverse.io by gharsh11032000 */
+    .button {
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 5px;
+        padding: 15px 20px;
+        background-color: #212121;
+        border: none;
+        font: inherit;
+        color: #e8e8e8;
+        font-size: 20px;
+        font-weight: 600;
+        border-radius: 50px;
+        cursor: not-allowed;
+        overflow: hidden;
+        transition: all 0.3s ease cubic-bezier(0.23, 1, 0.320, 1);
+    }
+
+    .button span {
+        position: relative;
+        z-index: 2;
+        display: flex;
+        align-items: center;
+    }
+
+    .button::before {
+        position: absolute;
+        content: '';
+        width: 100%;
+        height: 100%;
+        translate: 0 105%;
+        background-color: #F53844;
+        transition: all 0.3s cubic-bezier(0.23, 1, 0.320, 1);
+    }
+
+    .button svg {
+        width: 32px;
+        height: 32px;
+        fill: #F53844;
+        transition: all 0.3s cubic-bezier(0.23, 1, 0.320, 1);
+    }
+
+    .button:hover {
+        animation: shake 0.2s linear 1;
+    }
+
+    .button:hover::before {
+        translate: 0 0;
+    }
+
+    .button:hover svg {
+        fill: #e8e8e8;
+    }
+
+    @keyframes shake {
+        0% {
+            rotate: 0deg;
+        }
+
+        33% {
+            rotate: 10deg;
+        }
+
+        66% {
+            rotate: -10deg;
+        }
+
+        100% {
+            rotate: 10deg;
+        }
     }
 </style>
 @endsection
@@ -34,15 +108,46 @@
 <li><a href="/">Inicio</a></li>
 <li><a href="/acerca-de">Acerca de</a></li>
 <li><a href="/galeria-de-fotos" class="active">Galeria de fotos</a></li>
-<li class="dropdown"><a href="#"><span>Practicar</span> <i
-            class="bi bi-chevron-down toggle-dropdown"></i></a>
+@if (Route::has('login'))
+
+@auth
+<li><a href="{{ url('/home') }}">
+        Empezar a practicar
+    </a></li>
+@else
+<li class="dropdown"><a href="#"><span>Practicar</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
     <ul>
         <li><a href="/login">Iniciar sesión</a></li>
+        @if (Route::has('register'))
         <li><a href="/register">Crear una cuenta</a></li>
         <li><a href="#">Jugar como invitado</a></li>
+        @endif
     </ul>
 </li>
-<li><a href="/">Contacto</a></li>
+@endauth
+@endif
+<li>
+    <label class="theme-switch" style="margin-left: 15px;">
+        <input type="checkbox" class="theme-switch__checkbox">
+        <div class="theme-switch__container">
+            <div class="theme-switch__clouds"></div>
+            <div class="theme-switch__stars-container">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144 55" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M135.831 3.00688C135.055 3.85027 134.111 4.29946 133 4.35447C134.111 4.40947 135.055 4.85867 135.831 5.71123C136.607 6.55462 136.996 7.56303 136.996 8.72727C136.996 7.95722 137.172 7.25134 137.525 6.59129C137.886 5.93124 138.372 5.39954 138.98 5.00535C139.598 4.60199 140.268 4.39114 141 4.35447C139.88 4.2903 138.936 3.85027 138.16 3.00688C137.384 2.16348 136.996 1.16425 136.996 0C136.996 1.16425 136.607 2.16348 135.831 3.00688ZM31 23.3545C32.1114 23.2995 33.0551 22.8503 33.8313 22.0069C34.6075 21.1635 34.9956 20.1642 34.9956 19C34.9956 20.1642 35.3837 21.1635 36.1599 22.0069C36.9361 22.8503 37.8798 23.2903 39 23.3545C38.2679 23.3911 37.5976 23.602 36.9802 24.0053C36.3716 24.3995 35.8864 24.9312 35.5248 25.5913C35.172 26.2513 34.9956 26.9572 34.9956 27.7273C34.9956 26.563 34.6075 25.5546 33.8313 24.7112C33.0551 23.8587 32.1114 23.4095 31 23.3545ZM0 36.3545C1.11136 36.2995 2.05513 35.8503 2.83131 35.0069C3.6075 34.1635 3.99559 33.1642 3.99559 32C3.99559 33.1642 4.38368 34.1635 5.15987 35.0069C5.93605 35.8503 6.87982 36.2903 8 36.3545C7.26792 36.3911 6.59757 36.602 5.98015 37.0053C5.37155 37.3995 4.88644 37.9312 4.52481 38.5913C4.172 39.2513 3.99559 39.9572 3.99559 40.7273C3.99559 39.563 3.6075 38.5546 2.83131 37.7112C2.05513 36.8587 1.11136 36.4095 0 36.3545ZM56.8313 24.0069C56.0551 24.8503 55.1114 25.2995 54 25.3545C55.1114 25.4095 56.0551 25.8587 56.8313 26.7112C57.6075 27.5546 57.9956 28.563 57.9956 29.7273C57.9956 28.9572 58.172 28.2513 58.5248 27.5913C58.8864 26.9312 59.3716 26.3995 59.9802 26.0053C60.5976 25.602 61.2679 25.3911 62 25.3545C60.8798 25.2903 59.9361 24.8503 59.1599 24.0069C58.3837 23.1635 57.9956 22.1642 57.9956 21C57.9956 22.1642 57.6075 23.1635 56.8313 24.0069ZM81 25.3545C82.1114 25.2995 83.0551 24.8503 83.8313 24.0069C84.6075 23.1635 84.9956 22.1642 84.9956 21C84.9956 22.1642 85.3837 23.1635 86.1599 24.0069C86.9361 24.8503 87.8798 25.2903 89 25.3545C88.2679 25.3911 87.5976 25.602 86.9802 26.0053C86.3716 26.3995 85.8864 26.9312 85.5248 27.5913C85.172 28.2513 84.9956 28.9572 84.9956 29.7273C84.9956 28.563 84.6075 27.5546 83.8313 26.7112C83.0551 25.8587 82.1114 25.4095 81 25.3545ZM136 36.3545C137.111 36.2995 138.055 35.8503 138.831 35.0069C139.607 34.1635 139.996 33.1642 139.996 32C139.996 33.1642 140.384 34.1635 141.16 35.0069C141.936 35.8503 142.88 36.2903 144 36.3545C143.268 36.3911 142.598 36.602 141.98 37.0053C141.372 37.3995 140.886 37.9312 140.525 38.5913C140.172 39.2513 139.996 39.9572 139.996 40.7273C139.996 39.563 139.607 38.5546 138.831 37.7112C138.055 36.8587 137.111 36.4095 136 36.3545ZM101.831 49.0069C101.055 49.8503 100.111 50.2995 99 50.3545C100.111 50.4095 101.055 50.8587 101.831 51.7112C102.607 52.5546 102.996 53.563 102.996 54.7273C102.996 53.9572 103.172 53.2513 103.525 52.5913C103.886 51.9312 104.372 51.3995 104.98 51.0053C105.598 50.602 106.268 50.3911 107 50.3545C105.88 50.2903 104.936 49.8503 104.16 49.0069C103.384 48.1635 102.996 47.1642 102.996 46C102.996 47.1642 102.607 48.1635 101.831 49.0069Z" fill="currentColor"></path>
+                </svg>
+            </div>
+            <div class="theme-switch__circle-container">
+                <div class="theme-switch__sun-moon-container">
+                    <div class="theme-switch__moon">
+                        <div class="theme-switch__spot"></div>
+                        <div class="theme-switch__spot"></div>
+                        <div class="theme-switch__spot"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </label>
+</li>
 @endsection
 
 @section('content')
@@ -53,24 +158,24 @@
         <div class="row justify-content-center">
             <div class="col-lg-8">
                 <h2>Pagina en construcción</h2>
-                <p>Estamos trabajando en ello. Pronto se actualizara la pagina!</p>
+                <p style="color: white;">Estamos trabajando en ello. Pronto se actualizara la pagina!</p>
             </div>
-            <div class="countdown d-flex justify-content-center" data-count="2025/12/3">
+            <div class="countdown d-flex justify-content-center" data-count="2025/12/3" style="color: white;">
                 <div>
-                    <h3 class="count-days">482</h3>
-                    <h4>Días</h4>
+                    <h3 class="count-days" style="color: white;">482</h3>
+                    <h4 style="color: white;">Días</h4>
                 </div>
                 <div>
-                    <h3 class="count-hours">6</h3>
-                    <h4>Horas</h4>
+                    <h3 class="count-hours" style="color: white;">6</h3>
+                    <h4 style="color: white;">Horas</h4>
                 </div>
                 <div>
-                    <h3 class="count-minutes">49</h3>
-                    <h4>Minutos</h4>
+                    <h3 class="count-minutes" style="color: white;">49</h3>
+                    <h4 style="color: white;">Minutos</h4>
                 </div>
                 <div>
-                    <h3 class="count-seconds">41</h3>
-                    <h4>Segundos</h4>
+                    <h3 class="count-seconds" style="color: white;">41</h3>
+                    <h4 style="color: white;">Segundos</h4>
                 </div>
             </div>
 
@@ -84,10 +189,16 @@
                 </form>
             </div> -->
 
-            <div class="social-links">
-                <a href="#"><i class="bi bi-twitter-x"></i></a>
-                <a href="#"><i class="bi bi-facebook"></i></a>
-                <a href="#"><i class="bi bi-instagram"></i></a>
+            <div style="max-width: 200px;">
+                <a href="/" class="button">
+                    <span>Regresar
+                    </span>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-miterlimit="2" stroke-linejoin="round" fill-rule="evenodd" clip-rule="evenodd">
+                            <path fill-rule="nonzero" d="m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 1.5c-4.69 0-8.497 3.807-8.497 8.497s3.807 8.498 8.497 8.498 8.498-3.808 8.498-8.498-3.808-8.497-8.498-8.497zm0 7.425 2.717-2.718c.146-.146.339-.219.531-.219.404 0 .75.325.75.75 0 .193-.073.384-.219.531l-2.717 2.717 2.727 2.728c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.384-.073-.53-.219l-2.729-2.728-2.728 2.728c-.146.146-.338.219-.53.219-.401 0-.751-.323-.751-.75 0-.192.073-.384.22-.531l2.728-2.728-2.722-2.722c-.146-.147-.219-.338-.219-.531 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"></path>
+                        </svg>
+                    </span>
+                </a>
             </div>
 
         </div>
